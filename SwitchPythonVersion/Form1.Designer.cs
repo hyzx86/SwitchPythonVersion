883015727b9efade9532bc98c70e9e798e36412c
hyz <huarui2219@qq.com>
2017-12-21 23:54:17 +0800

    Version 0.1

--- SwitchPythonVersion/Form1.Designer.cs
+++ SwitchPythonVersion/Form1.Designer.cs
@@ -28,12 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Python2.x Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Python3.x Path";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(293, 21);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(516, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Switch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(516, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Switch";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(62, 234);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(433, 119);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Current Path Value:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(168, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "如： C:\\Python27";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(166, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "如： C:\\Python3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(516, 234);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 119);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Manually set";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(62, 360);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(71, 12);
+            this.lblMsg.TabIndex = 11;
+            this.lblMsg.Text = "Path Saved!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(661, 399);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Switch Python Version Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblMsg;
     }
 }
 
--- SwitchPythonVersion/Form1.cs
+++ SwitchPythonVersion/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,9 +12,114 @@ namespace SwitchPythonVersion
 {
     public partial class Form1 : Form
     {
+        private bool loading = true;
         public Form1()
         {
             InitializeComponent();
+            lblMsg.Text = string.Empty;
+            if (File.Exists("SwitchPythonVersion.config"))
+            {
+                using (var fs = File.Open("SwitchPythonVersion.config", FileMode.Open))
+                {
+                    var sr = new StreamReader(fs);
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine();
+                        if (line.StartsWith("Py2x"))
+                        {
+                            this.textBox1.Text = line.Replace("Py2x=", string.Empty);
+                        }
+                        if (line.StartsWith("Py3x"))
+                        {
+                            this.textBox2.Text = line.Replace("Py3x=", string.Empty);
+                        }
+                    }
+                    sr.Close();
+                }
+            }
+            loading = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox3.Text = LoadPath();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var TargetPath = textBox1.Text.Trim();
+            var OldPath = textBox2.Text.Trim();
+
+            SwitchPy(TargetPath, OldPath);
+
+        }
+
+        private void SwitchPy(string TargetPath, string OldPath)
+        {
+            if (!string.IsNullOrEmpty(TargetPath) && !string.IsNullOrEmpty(OldPath))
+            {
+                textBox3.Text = LoadPath();
+                var pathList = textBox3.Text.Split(';');
+
+                for (int i = 0; i < pathList.Length; i++)
+                {
+                    var item = pathList[i];
+                    if (item.ToLower().Trim().StartsWith(OldPath.ToLower()))
+                    {
+                        item = item.Replace(OldPath, TargetPath);
+                    }
+                    pathList[i] = item;
+                }
+                var newPathValue = string.Join(";", pathList);
+                SysEnvironment.SetSysEnvironment("PATH", newPathValue);
+                lblMsg.Text = "PATH Saved! - - " + DateTime.Now.ToString();
+                textBox3.Text = LoadPath();
+            }
+            else
+            {
+                BB();
+            }
+        }
+
+        private void BB()
+        {
+            MessageBox.Show("路径都不写，你想上天啊？放心吧，你写好了，哥会提你保存的。");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var TargetPath = textBox2.Text.Trim();
+            var OldPath = textBox1.Text.Trim();
+
+            SwitchPy(TargetPath, OldPath);
+        }
+
+
+        private string LoadPath()
+        {
+            return SysEnvironment.GetSysEnvironmentByName("PATH");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!loading)
+            {
+                using (var fs = File.Open("SwitchPythonVersion.config", FileMode.Create))
+                {
+                    var sw = new StreamWriter(fs);
+                    sw.WriteLine("Py2x=" + this.textBox1.Text);
+                    sw.WriteLine("Py3x=" + this.textBox2.Text);
+                    sw.Close();
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SysEnvironment.SetSysEnvironment("PATH",textBox3.Text);
+            lblMsg.Text = "PATH Saved! - - " + DateTime.Now.ToString();
         }
     }
 }
